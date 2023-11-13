ae8e88377cb36b7a1f9a4e6e1c5501585e1a15f6
Tom Serenander <tom@serenander.se>
2023-11-13 10:10:28 +0100

    Generate 10 random dice.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import { useState } from 'react'
+import Die from './Die'
+
+function allNewDice() {
+	const randomArray = []
+	for (let i = 0; i < 10; i++) {
+		randomArray[i] = Math.ceil(6 * Math.random())
+	}
+	return randomArray
+}
+
+allNewDice()
 
 function App() {
-	return <main></main>
+	const dice = allNewDice().map((die) => <Die value={die} />)
+	return (
+		<main>
+			<div className="container">{dice}</div>
+		</main>
+	)
 }
 
 export default App
---
+++ src/Die.jsx
@@ -0,0 +1,7 @@
+export default function Die(props) {
+	return (
+		<div className="die--face" id="die--face">
+			<h2>{props.value}</h2>
+		</div>
+	)
+}
