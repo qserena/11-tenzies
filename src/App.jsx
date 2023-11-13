68301e24c2dd5d84d720a5c21608524f9aba9c2f
Tom Serenander <tom@serenander.se>
2023-11-13 15:07:57 +0100

    Possible to hold die.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,55 @@
 import { useState } from 'react'
 import Die from './Die'
-
-function allNewDice() {
-	const randomArray = []
-	for (let i = 0; i < 10; i++) {
-		randomArray[i] = Math.ceil(6 * Math.random())
-	}
-	return randomArray
-}
-
-allNewDice()
+import { nanoid } from 'nanoid'
 
 function App() {
-	const dice = allNewDice().map((die) => <Die value={die} />)
+	const [dice, setDice] = useState(allNewDice())
+
+	function allNewDice() {
+		const diceArray = []
+		for (let i = 0; i < 10; i++) {
+			diceArray[i] = {
+				value: Math.ceil(6 * Math.random()),
+				isHeld: false,
+				id: nanoid(),
+			}
+		}
+		return diceArray
+	}
+
+	function rollDice(e) {
+		setDice((prevDice) =>
+			prevDice.map((die) => ({
+				...die,
+				value: die.isHeld ? die.value : Math.ceil(6 * Math.random()),
+			}))
+		)
+	}
+
+	function holdDice(id) {
+		setDice((prevDice) =>
+			prevDice.map((die) => ({
+				...die,
+				isHeld: die.id === id ? !die.isHeld : die.isHeld,
+			}))
+		)
+	}
+
+	const diceElements = dice.map((die) => (
+		<Die
+			key={die.id}
+			value={die.value}
+			isHeld={die.isHeld}
+			handleClick={() => holdDice(die.id)}
+		/>
+	))
+
 	return (
 		<main>
-			<div className="container">{dice}</div>
+			<div className="dice-container">{diceElements}</div>
+			<button className="roll-btn" id="roll-btn" onClick={rollDice}>
+				Roll
+			</button>
 		</main>
 	)
 }
